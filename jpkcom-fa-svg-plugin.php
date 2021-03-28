bfdf2e3fc2945af2c9fecd1631ae4caa798a55f0
Jean Pierre Kolb <jp.kolb@creationell.de>
2021-03-28 13:50:26 +0200

    Updated plugin file

--- jpkcom-fa-svg-plugin.php
+++ jpkcom-fa-svg-plugin.php
@@ -14,5 +14,5 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'JPKCOM_WPTHEME_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-define( 'JPKCOM_WPTHEME_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'JPKCOM_FASVG_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'JPKCOM_FASVG_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
